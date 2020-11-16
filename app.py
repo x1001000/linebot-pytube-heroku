30251a628033c758a86d7bdb87438d17495a57b5
x1001000 <x1001000@gmail.com>
2020-11-17 04:29:25 +0800

    yt

--- app.py
+++ app.py
@@ -5,6 +5,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    print("TEST")
-    YouTube('https://youtu.be/T1dcfK10K8g').streams.first().download(filename='ytdl')
+    print(YouTube('https://youtu.be/T1dcfK10K8g').streams.first().download(filename='ytdl'))
     return "Hello World!"
