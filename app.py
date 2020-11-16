a3b8e6d896c5cc8a7c1ce16af5b98bed0c76274e
x1001000 <x1001000@gmail.com>
2020-11-17 04:10:28 +0800

    yt

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from pytube import YouTube
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    YouTube('https://youtu.be/T1dcfK10K8g').streams.first().download(filename='ytdl')
     return "Hello World!"
