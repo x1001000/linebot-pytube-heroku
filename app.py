e219262b8e5d3436277126c0301edff6d92f66d1
x1001000 <x1001000@gmail.com>
2020-11-17 15:56:25 +0800

    d

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, send_file
+from flask import Flask
 from pytube import YouTube
+from moviepy.editor import *
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     print(YouTube('https://youtu.be/T1dcfK10K8g').streams.first().download(output_path='static',filename='ytdl'))
-    return 'static/ytdl.mp4'
+    video = VideoFileClip('static/ytdl.mp4')
+    audio = video.audio
+    audio.write_audiofile('static/line.mp3')
+    video.close()
+    audio.close()
+    return 'OK'
