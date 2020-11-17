b4ce68cb5d28e4ee675da9329141f5781f9e3f74
x1001000 <x1001000@gmail.com>
2020-11-17 13:54:10 +0800

    d

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    print(YouTube('https://youtu.be/T1dcfK10K8g').streams.first().download(filename='static/ytdl'))
+    print(YouTube('https://youtu.be/T1dcfK10K8g').streams.first().download(output_path='static',filename='ytdl'))
     return 'static/ytdl.mp4'
